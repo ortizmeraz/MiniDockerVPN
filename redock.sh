7ca71fc23a80774783bcff9205f57de2cb7c4e74
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-06-25 11:24:08 -0400

    Update redock.sh

--- redock.sh
+++ redock.sh
@@ -2,14 +2,17 @@
 
 # stop & remove all containers
 docker stop "$(docker ps -aq)"
-sleep 1
 docker rm   "$(docker ps -aq)"
-sleep 1
+sleep 5
 sudo rm DockerConfs/gluetun/servers.json
 # bring up your stacks
 docker-compose -f docker/portainer-compose.yml up -d --remove-orphans
 sleep 1
 docker-compose -f docker/VPN_compose.yml      up -d --remove-orphans
-sleep 1
+sleep 5
 docker-compose -f docker/Services-compose.yml up -d --remove-orphans
+sleep 1
+docker-compose -f docker/podgrab.yml up -d --remove-orphans
+sleep 1
+docker-compose -f docker/Syncthing.yml up -d --remove-orphans
 sleep 1
